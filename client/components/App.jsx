6084c7456a14c323b7eeed2533ebd0fc7a0fc060
Lara Hayward <haywardlbl@gmail.com>
2021-02-13 17:21:16 +1300

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchFruits } from '../actions'
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(fetchFruits())
+  })
+
+  return (
+    <>
+      <div className='app'>
+        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <ul>
+          {props.fruits.map(fruit => (
+            <li key={fruit}>{fruit}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+const mapStateToProps = (globalState) => {
+  return {
+    fruits: globalState.fruits
+  }
+}
+
+export default connect(mapStateToProps)(App)
