63d49874ca9fb4214bf380b65195c76264821da7
Lara Hayward <haywardlbl@gmail.com>
2021-03-23 14:07:53 +1300

    slight refactor to make if statement for existing user match more readable

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -10,15 +10,24 @@ const Landing = () => {
   const [userType, setUserType] = useState(null)
   const [error, setError] = useState(null)
 
+  useEffect(() => {
+    findUsers()
+  }, [])
+
   const findUsers = () => {
     getUsers().then((u) => {
       setUsers(() => u.map((uu) => uu.username))
     })
   }
-
-  useEffect(() => {
-    findUsers()
-  }, [])
+  const makeNewUser = (name) => {
+    if (users.find((user) => user == name)) {
+      setError("userAlreadyExists")
+    } else {
+      localStorage.setItem("user", name)
+      newUser(name)
+      history.push("/make")
+    }
+  }
 
   const clickHandler = (type) => {
     setUserType(() => type)
@@ -32,32 +41,27 @@ const Landing = () => {
     setUserType(() => "existingUserNotLocalStorage")
   }
 
-  const makeNewUser = (name) => {
-    if(users.find(user => user == name)){
-      setError("userAlreadyExists")
-    } else {
-      localStorage.setItem("user", name)
-      newUser(name)
-      history.push("/make")
-    }
-  }
-
   const submitHander = (e) => {
     e.preventDefault()
     setError(null)
     setBestMatch(null)
     if (userType == "newUser") {
       makeNewUser(e.target.username.value)
-    } else if (userType == "existingUser" || userType == "existingUserNotLocalStorage") {
-      const matches = findBestMatch(e.target.username.value, users)
-      console.log(matches.bestMatch.rating)
+    } else if (
+      userType == "existingUser" ||
+      userType == "existingUserNotLocalStorage"
+    ) {
+      findMatch(e.target.username.value)
+    }
+
+    const findMatch = (usersname) => {
+      const matches = findBestMatch(usersname, users)
       if (matches.bestMatch.rating == 1) {
-        localStorage.setItem("user", e.target.username.value)
+        localStorage.setItem("user", usersname)
         history.push("/make")
-      } else if(matches.bestMatch.rating < 0.5){
+      } else if (matches.bestMatch.rating < 0.5) {
         setError("noCloseMatch")
-      }
-      else {
+      } else {
         setBestMatch(users[matches.bestMatchIndex])
       }
     }
@@ -74,7 +78,7 @@ const Landing = () => {
 
   return (
     <>
-     {console.log(userType)}
+      {console.log(userType)}
       <div>
         <button onClick={() => clickHandler("newUser")}>New User</button>
         <button onClick={() => clickHandler("existingUser")}>
@@ -82,18 +86,22 @@ const Landing = () => {
         </button>
       </div>
 
-      {userType != null ? localStorage.getItem("user") == null ? (
-        <form onSubmit={submitHander}>
-          <input type="text" name="username" autoComplete="off"></input>
-          <button>Go!</button>
-        </form>
+      {userType != null ? (
+        localStorage.getItem("user") == null ? (
+          <form onSubmit={submitHander}>
+            <input type="text" name="username" autoComplete="off"></input>
+            <button>Go!</button>
+          </form>
+        ) : (
+          <div>
+            Are you {localStorage.getItem("user")}?
+            <button onClick={localStorageSuccessHandler}>Yes!</button>
+            <button onClick={clickHandlerNotLocalStorage}>No :(</button>
+          </div>
+        )
       ) : (
-        <div>
-          Are you {localStorage.getItem("user")}?
-          <button onClick={localStorageSuccessHandler}>Yes!</button>
-          <button onClick={clickHandlerNotLocalStorage}>No :(</button>
-        </div>
-      ) : <></>}
+        <></>
+      )}
 
       {bestMatch && (
         <div>
@@ -101,8 +109,10 @@ const Landing = () => {
           <button onClick={() => setBestMatch(null)}>No :(</button>
         </div>
       )}
-      {error == "noCloseMatch" && <p>No match to existing user! Please try again!</p>}
-      {error == "userAlreadyExists" && <p>That username already exists!</p> }
+      {error == "noCloseMatch" && (
+        <p>Sorry, we can't find a match to your name. Please try again!</p>
+      )}
+      {error == "userAlreadyExists" && <p>That name already exists!</p>}
     </>
   )
 }
